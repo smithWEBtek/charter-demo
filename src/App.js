b88cc91c8725287dacab62ffff9269ca64a37473
brad@smithwebtek.com <brad@smithwebtek.com>
2022-09-03 22:10:48 -0400

    Draft v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Customer from './Customer/Customer';
+import classes from './App.modules.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({});
+  const url = 'data.json'
+  const getData = () => {
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log('error: ', error))
+  };
+
+  // useEffect(() => {
+  //   getData()
+  // }, [])
+
+  const renderedCustomerPurchases = data.customers?.map(customer => {
+    return <Customer customer={customer} key={customer.id} />
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className={classes.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Charter_Communications_logo.svg/2880px-Charter_Communications_logo.svg.png" alt="charter-communications-logo" />
+      <h3>Customer reward points</h3>
+      <button data-testid="data-button" onClick={() => getData()}>click for data</button>
+      {renderedCustomerPurchases}
     </div>
   );
 }
